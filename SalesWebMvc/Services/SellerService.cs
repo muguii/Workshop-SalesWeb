a021d738d91e0b4295d0eb2b10f231d120d70d93
Muriel Aurelio <murielaaurelio@gmail.com>
2020-04-28 16:56:49 -0300

    Detalhes dos vendedores

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,7 @@ using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace SalesWebMVC.Services
@@ -29,7 +30,7 @@ namespace SalesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(x => x.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
         }
 
         public void Remove(int id)
